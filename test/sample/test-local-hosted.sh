ddad64d9583eab0595dd33f5854cba69892b1bcf
Dhanar Adi Dewandaru <dhanar@accelbyte.net>
2024-10-11 13:32:12 +0700

    test(sample): local hosted - increase timeout

--- test/sample/test-local-hosted.sh
+++ test/sample/test-local-hosted.sh
@@ -22,8 +22,8 @@ python -m pip install -r requirements.txt
 (cd gateway && go build -buildvcs=false -o gateway && BASE_PATH=/$APP_BASE_PATH ./gateway) & GATEWAY_PID=$!
 (cd src && python -m app) & SERVICE_PID=$!
 
-(for _ in {1..12}; do bash -c "timeout 1 echo > /dev/tcp/127.0.0.1/8000" 2>/dev/null && exit 0 || sleep 5s; done; exit 1)
-(for _ in {1..12}; do bash -c "timeout 1 echo > /dev/tcp/127.0.0.1/8080" 2>/dev/null && exit 0 || sleep 5s; done; exit 1)
+(for _ in {1..12}; do bash -c "timeout 1 echo > /dev/tcp/127.0.0.1/8000" 2>/dev/null && exit 0 || sleep 10s; done; exit 1)
+(for _ in {1..12}; do bash -c "timeout 1 echo > /dev/tcp/127.0.0.1/8080" 2>/dev/null && exit 0 || sleep 10s; done; exit 1)
 
 if [ $? -ne 0 ]; then
   echo "Failed to run Extend app locally"
